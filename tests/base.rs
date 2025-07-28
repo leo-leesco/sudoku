ef2dcd1a5604d263936521f81570a5eb908f05a7
Léo LEESCO <leo.leesco@gmail.com>
2025-07-27 21:01:58 -0700

    plausible trivial first step

--- tests/base.rs
+++ tests/base.rs
@@ -1,6 +1,6 @@
 use sudoku::*;
 
-const VALID: Sudoku = sudoku![
+const VALID: Sudoku<Option<u8>> = sudoku![
     [5, 9, 7, 8, 3, 2, 1, 6, 4],
     [8, 2, 1, 4, 6, 9, 7, 3, 5],
     [3, 6, 4, 5, 7, 1, 2, 8, 9],
@@ -12,7 +12,19 @@ const VALID: Sudoku = sudoku![
     [9, 4, 6, 1, 2, 7, 3, 5, 8]
 ];
 
-const WITH_HOLES: Sudoku = sudoku![
+const MISS_ONE: Sudoku<Option<u8>> = sudoku![
+    [0, 9, 7, 8, 3, 2, 1, 6, 4],
+    [8, 2, 1, 4, 6, 9, 7, 3, 5],
+    [3, 6, 4, 5, 7, 1, 2, 8, 9],
+    [4, 5, 9, 2, 8, 3, 6, 7, 1],
+    [1, 8, 3, 7, 4, 6, 5, 9, 2],
+    [6, 7, 2, 9, 1, 5, 8, 4, 3],
+    [7, 1, 5, 3, 9, 8, 4, 2, 6],
+    [2, 3, 8, 6, 5, 4, 9, 1, 7],
+    [9, 4, 6, 1, 2, 7, 3, 5, 8]
+];
+
+const WITH_HOLES: Sudoku<Option<u8>> = sudoku![
     [5, 0, 0, 0, 0, 2, 1, 0, 4],
     [8, 0, 0, 4, 0, 0, 0, 3, 0],
     [3, 6, 0, 0, 0, 0, 0, 0, 0],
@@ -24,7 +36,7 @@ const WITH_HOLES: Sudoku = sudoku![
     [9, 0, 6, 1, 0, 0, 0, 0, 8]
 ];
 
-const DEFAULT: Sudoku = sudoku![
+const DEFAULT: Sudoku<Option<u8>> = sudoku![
     [1, 2, 3, 4, 5, 6, 7, 8, 9],
     [2, 3, 4, 5, 6, 7, 8, 9, 1],
     [3, 4, 5, 6, 7, 8, 9, 1, 2],
@@ -74,4 +86,135 @@ fn assert_valid() {
 #[test]
 fn assert_invalid() {
     assert!(!valid(DEFAULT))
+}
+
+#[test]
+fn test_barre() {
+    assert_eq!(barre(VALID, 3), [[false; SIZE]; SIZE]);
+    assert_eq!(
+        barre(WITH_HOLES, 2),
+        [
+            [
+                false, false, false, false, false, false, false, false, false
+            ],
+            [false, true, false, false, false, false, true, false, true],
+            [false, false, false, false, false, false, true, false, true],
+            [false, false, false, true, false, false, false, false, true],
+            [false, false, false, true, true, false, true, false, true],
+            [
+                false, false, false, false, false, false, false, false, false
+            ],
+            [
+                false, false, false, false, false, false, false, false, false
+            ],
+            [true, false, false, true, true, false, false, false, false],
+            [false, true, false, false, true, false, false, false, false]
+        ]
+    )
+    // println!("{:?}", barre(WITH_HOLES, 2)) // to get the output via `cargo test -- --nocapture`
+}
+
+#[test]
+fn test_easy_one_step_trivial() {
+    assert_eq!(one_step_trivial(MISS_ONE), Ok(VALID))
+}
+
+#[test]
+fn test_one_step_trivial() {
+    assert_eq!(
+        one_step_trivial(WITH_HOLES),
+        Ok([
+            [
+                Some(5),
+                None,
+                None,
+                None,
+                None,
+                Some(2),
+                Some(1),
+                Some(6),
+                Some(4)
+            ],
+            [
+                Some(8),
+                Some(2),
+                None,
+                Some(4),
+                None,
+                None,
+                None,
+                Some(3),
+                None
+            ],
+            [
+                Some(3),
+                Some(6),
+                Some(4),
+                None,
+                None,
+                None,
+                None,
+                None,
+                None
+            ],
+            [
+                None,
+                None,
+                None,
+                None,
+                Some(8),
+                Some(3),
+                Some(6),
+                None,
+                None
+            ],
+            [
+                None,
+                Some(8),
+                Some(3),
+                None,
+                Some(4),
+                None,
+                None,
+                Some(9),
+                None
+            ],
+            [
+                None,
+                None,
+                Some(2),
+                Some(9),
+                Some(1),
+                None,
+                None,
+                None,
+                None
+            ],
+            [None, None, None, None, None, None, None, Some(2), Some(6)],
+            [
+                None,
+                Some(3),
+                None,
+                None,
+                None,
+                Some(4),
+                None,
+                None,
+                Some(7)
+            ],
+            [
+                Some(9),
+                None,
+                Some(6),
+                Some(1),
+                None,
+                None,
+                None,
+                None,
+                Some(8)
+            ]
+        ])
+    );
+
+    // println!("{:?}", one_step_trivial(WITH_HOLES))
 }
